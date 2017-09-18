25616fb08d72521e6b3a6829e0c76f4520864f22
Robert Foy <robert.foy@gmail.com>
2017-09-18 21:01:46 +1000

    Update 1337x.to.user.js

--- 1337x.to.user.js
+++ 1337x.to.user.js
@@ -9,10 +9,17 @@
 
 const title = encodeURIComponent(document.title.replace(' - IMDb', ''));
 const overview = document.getElementById('title-overview-widget');
+let { parentElem, siblingElem } = { parentElem: overview.parentNode, siblingElem: overview.nextSibling };
 GM_xmlhttpRequest({
   method: "GET",
   url: `https://1337x.to/category-search/${title}/Movies/1/`,
-  onload: response => overview.parentNode.insertBefore(new DOMParser()
-    .parseFromString(response.responseText, "text/html").getElementsByClassName('table-list-wrap')[0],
-    overview.nextSibling)
+  onload: response => {
+    let content = new DOMParser().parseFromString(response.responseText, "text/html");
+    let linksTable = content.getElementsByClassName('table-list-wrap')[0];
+    ['coll-date', 'coll-5', 'coll-4'].forEach(cls => Array.from(linksTable.getElementsByClassName(cls)).forEach(col => col.remove()));
+    linksTable.querySelector('th.coll-1').textContent = 'Name';
+    linksTable.querySelector('th.coll-2').textContent = 'Seeders';
+    linksTable.querySelector('th.coll-3').textContent = 'Leechers';
+    parentElem.insertBefore(linksTable, siblingElem);
+  }
 });
